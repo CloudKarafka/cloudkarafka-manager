a2610f57c97df2c7fb3899548dfa3b63b4a7cf2f
Magnus Hörberg <magnus.hoerberg@gmail.com>
2017-08-30 15:05:57 +0200

    calculate coverage and lag for consumers

--- server/api/consumers.go
+++ server/api/consumers.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"cloudkarafka-mgmt/jmx"
 	"cloudkarafka-mgmt/kafka"
 	"cloudkarafka-mgmt/zookeeper"
 
@@ -8,11 +9,18 @@ import (
 
 	"fmt"
 	"net/http"
+	"strconv"
 )
 
 type consumerView struct {
-	Name   string   `json:"name"`
-	Topics []string `json:"topics"`
+	Name   string              `json:"name"`
+	Topics []consumedTopicView `json:"topics"`
+}
+
+type consumedTopicView struct {
+	Name         string `json:"name"`
+	ConsumeRatio int    `json:"consume_ratio"`
+	Lag          int    `json:"lag"`
 }
 
 func Consumers(w http.ResponseWriter, r *http.Request, p zookeeper.Permissions) {
@@ -21,16 +29,27 @@ func Consumers(w http.ResponseWriter, r *http.Request, p zookeeper.Permissions) 
 
 func Consumer(w http.ResponseWriter, r *http.Request, p zookeeper.Permissions) {
 	vars := mux.Vars(r)
-	consumer := kafka.Consumer(vars["name"], p)
-	fmt.Println(consumer)
-	if consumer == nil {
+	cts := kafka.Consumer(vars["name"], p)
+	if cts == nil {
 		http.NotFound(w, r)
 		return
 	}
-	var topics []string
-	for t, _ := range consumer {
+	var topics []consumedTopicView
+	for t, cps := range cts {
 		if p.TopicRead(t) {
-			topics = append(topics, t)
+			lag := 0
+			for part, off := range cps {
+				e, err := jmx.LogOffset("LogEndOffset", t, strconv.Itoa(part))
+				if err != nil {
+					fmt.Println(err)
+				}
+				lag += (e - off.Offset)
+			}
+			topics = append(topics, consumedTopicView{
+				Name:         t,
+				Lag:          lag,
+				ConsumeRatio: int((len(cps) / 10) * 100),
+			})
 		}
 	}
 	writeJson(w, consumerView{Name: vars["name"], Topics: topics})
