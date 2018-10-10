14e130bf208c70a87d17b35300638ac25f1b4a24
Magnus Landerblom <magnus@landerblom.se>
2018-10-10 13:47:17 +0200

    More test for replica spreads

--- zookeeper/topics_test.go
+++ zookeeper/topics_test.go
@@ -122,6 +122,40 @@ func TestDecreaseReplicationOnPartitions(t *testing.T) {
 	verifySpread(t, expected, res)
 }
 
+func TestIncreaseReplicationOnPartitions(t *testing.T) {
+	pl := [][]int{
+		[]int{0, 1},
+		[]int{1, 0},
+		[]int{2, 1},
+		[]int{0, 2},
+	}
+	expected := [][]int{
+		[]int{0, 1, 2},
+		[]int{1, 0, 2},
+		[]int{2, 1, 0},
+		[]int{0, 2, 1},
+	}
+	brokers := []int{0, 1, 2}
+	res := updatePartitions(brokers, pl, 3, 4)
+	verifySpread(t, expected, res)
+}
+
+func TestAddPartitionAndIncreaseReplica(t *testing.T) {
+	pl := [][]int{
+		[]int{0, 1},
+		[]int{1, 0},
+	}
+	expected := [][]int{
+		[]int{0, 1, 2},
+		[]int{1, 0, 2},
+		[]int{2, 1, 0},
+		[]int{0, 2, 1},
+	}
+	brokers := []int{0, 1, 2}
+	res := updatePartitions(brokers, pl, 3, 4)
+	verifySpread(t, expected, res)
+}
+
 func TestSpreadLeader(t *testing.T) {
 	pl := partitionList{
 		[]int{0},
