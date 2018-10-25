d6e950905224993bd7c8e89938b50986c161b08c
Magnus Hörberg <magnus.hoerberg@gmail.com>
2018-10-25 12:18:16 +0200

    move topic routes to goji router

--- server/server.go
+++ server/server.go
@@ -3,130 +3,50 @@ package server
 import (
 	"github.com/84codes/cloudkarafka-mgmt/config"
 	"github.com/84codes/cloudkarafka-mgmt/server/api"
-	"github.com/84codes/cloudkarafka-mgmt/zookeeper"
-	"github.com/gorilla/mux"
+
+	"github.com/zenazn/goji"
 
 	_ "net/http/pprof"
 
 	"fmt"
+	"net"
 	"net/http"
-	"runtime"
-	"time"
 )
 
-type aclScopedHandler func(http.ResponseWriter, *http.Request, zookeeper.Permissions)
-
-func protected(fn aclScopedHandler) http.HandlerFunc {
+func serveFile(path string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		user, pass, ok := r.BasicAuth()
-		if zookeeper.SkipAuthentication() {
-			p := zookeeper.Permissions{Cluster: zookeeper.R, Username: "default"}
-			fn(w, r, p)
-			//fmt.Printf("[INFO] method=%s route=%s status=%s\n", r.Method, r.URL.Path, w.Header())
-		} else if zookeeper.SkipAuthenticationWithWrite() {
-			p := zookeeper.Permissions{
-				Cluster:  zookeeper.W,
-				Topics:   map[string]zookeeper.Permission{"*": zookeeper.RW},
-				Groups:   map[string]zookeeper.Permission{"*": zookeeper.RW},
-				Username: "default"}
-			fn(w, r, p)
-			//fmt.Printf("[INFO] method=%s route=%s status=%s\n", r.Method, r.URL.Path, w.Header())
-		} else if ok && zookeeper.ValidateScramLogin(user, pass) {
-			p := zookeeper.PermissionsFor(user)
-			fn(w, r, p)
-			//fmt.Printf("[INFO] method=%s route=%s status=%s\n", r.Method, r.URL.Path, w.Header())
-		} else {
-			w.Header().Set("WWW-Authenticate", "Basic realm=\"Restricted\"")
-			w.WriteHeader(http.StatusUnauthorized)
-		}
+		p := fmt.Sprintf("./static/%s", path)
+		fmt.Println(p)
+		http.ServeFile(w, r, p)
 	}
 }
 
-func protecedServeFile(r *mux.Router, path, file string) {
-	r.HandleFunc(path, protected(func(w http.ResponseWriter, req *http.Request, _ zookeeper.Permissions) {
-		http.ServeFile(w, req, "static/"+file)
-	}))
-}
-
-func apiRoutes(r *mux.Router) {
-	a := r.PathPrefix("/api").Subrouter()
-	a.HandleFunc("/acls", protected(api.Acls))
-	a.HandleFunc("/acls/{resource}/{name}/{principal}", protected(api.Acl))
-	a.HandleFunc("/brokers", protected(api.Brokers))
-	a.HandleFunc("/brokers/throughput", protected(api.AllBrokerThroughputTimeseries))
-	a.HandleFunc("/brokers/{id}", protected(api.Broker))
-	a.HandleFunc("/brokers/{id}/jvm", protected(api.BrokerJvm))
-	a.HandleFunc("/brokers/{id}/health", protected(api.BrokerHealth))
-	a.HandleFunc("/brokers/{id}/throughput", protected(api.BrokerThroughputTimeseries))
-	a.HandleFunc("/brokers/throughput", protected(api.AllBrokerThroughputTimeseries))
-	a.HandleFunc("/topics", protected(api.Topics))
-	a.HandleFunc("/topics/{topic}", protected(api.Topic))
-	a.HandleFunc("/topics/{topic}/config", protected(api.Config))
-	a.HandleFunc("/topics/{topic}/spread-partitions", protected(api.SpreadPartitions))
-	a.HandleFunc("/topics/{topic}/throughput", protected(api.TopicThroughput))
-	a.HandleFunc("/consumers", protected(api.Consumers))
-	a.HandleFunc("/consumers/{name}", protected(api.Consumer))
-	a.HandleFunc("/consumers/{name}/{topic}/total-lag-series", protected(api.TotalLagSeries))
-	a.HandleFunc("/whoami", protected(api.Whoami))
-	a.HandleFunc("/users", protected(api.Users))
-	a.HandleFunc("/users/{name}", protected(api.User))
-	a.HandleFunc("/notifications", protected(api.Notifications))
-	a.HandleFunc("/browse/{topic}", protected(api.Browser))
-	a.HandleFunc("/version", func(w http.ResponseWriter, _r *http.Request) {
-		api.WriteJson(w, map[string]string{
-			"version":    config.Version,
-			"git_commit": config.GitCommit,
-		})
-	})
-	a.HandleFunc("/debug/memory-usage", protected(func(w http.ResponseWriter, _r *http.Request, _p zookeeper.Permissions) {
-		var m runtime.MemStats
-		runtime.ReadMemStats(&m)
-		api.WriteJson(w, map[string]string{
-			"Alloc":       fmt.Sprintf("%v MiB", m.Alloc/1024/1024),
-			"TotalAlloc":  fmt.Sprintf("%v MiB", m.TotalAlloc/1024/1024),
-			"Sys":         fmt.Sprintf("%v MiB", m.Sys/1024/1024),
-			"NumGC":       fmt.Sprintf("%v", m.NumGC),
-			"NumRoutines": fmt.Sprintf("%d", runtime.NumGoroutine()),
-		})
-	}))
-}
-
-func serveFile(r *mux.Router, path, file string) {
-	r.HandleFunc(path, func(w http.ResponseWriter, req *http.Request) {
-		http.ServeFile(w, req, "static/"+file)
-	})
-}
-
 func Start() {
-	r := mux.NewRouter()
-	apiRoutes(r)
-
-	serveFile(r, "/", "index.html")
-	serveFile(r, "/topics", "topics/index.html")
-	serveFile(r, "/topics/add", "topics/add.html")
-	serveFile(r, "/topic/edit", "topic/edit.html")
-	serveFile(r, "/topic/details", "topic/details.html")
-	serveFile(r, "/topic/browse", "topic/browse.html")
-	serveFile(r, "/brokers", "brokers.html")
-	serveFile(r, "/broker/details", "broker/details.html")
-	serveFile(r, "/consumers", "consumers/index.html")
-	serveFile(r, "/consumer/details", "consumer/details.html")
-	serveFile(r, "/admin", "admin/index.html")
-	serveFile(r, "/users/add", "users/add.html")
-	serveFile(r, "/acls/add", "acls/add.html")
-	serveFile(r, "/login", "login.html")
+	goji.Handle("/api/*", api.Mux)
+	goji.Get("/", serveFile("index.html"))
+	goji.Get("/topics", serveFile("topics/index.html"))
+	goji.Get("/topics/add", serveFile("topics/add.html"))
+	goji.Get("/topic/edit", serveFile("topic/edit.html"))
+	goji.Get("/topic/details", serveFile("topic/details.html"))
+	goji.Get("/topic/browse", serveFile("topic/browse.html"))
+	goji.Get("/brokers", serveFile("brokers.html"))
+	goji.Get("/broker/details", serveFile("broker/details.html"))
+	goji.Get("/consumers", serveFile("consumers/index.html"))
+	goji.Get("/consumer/details", serveFile("consumer/details.html"))
+	goji.Get("/admin", serveFile("admin/index.html"))
+	goji.Get("/users/add", serveFile("users/add.html"))
+	goji.Get("/acls/add", serveFile("acls/add.html"))
+	goji.Get("/login", serveFile("login.html"))
 
 	http.Handle("/js/", http.FileServer(http.Dir("static/")))
 	http.Handle("/css/", http.FileServer(http.Dir("static/")))
 	http.Handle("/fonts/", http.FileServer(http.Dir("static/")))
 	http.Handle("/assets/", http.FileServer(http.Dir("static/")))
 
-	http.Handle("/", r)
-	s := &http.Server{
-		Addr:         fmt.Sprintf(":%s", config.Port),
-		ReadTimeout:  60 * time.Second,
-		WriteTimeout: 60 * time.Second,
-	}
 	fmt.Println("[INFO] Listening on Port", config.Port)
-	fmt.Println(s.ListenAndServe())
+	listener, err := net.Listen("tcp4", fmt.Sprintf(":%s", config.Port))
+	if err != nil {
+		fmt.Println(err)
+	}
+	goji.ServeListener(listener)
 }
