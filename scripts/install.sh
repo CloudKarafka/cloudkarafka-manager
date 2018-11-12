47c16291e8ba363301e3c491fbe68ec4a69e8018
Magnus Landerblom <magnus@landerblom.se>
2018-11-12 09:44:51 +0100

    Travis: sudo install

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,13 +2,13 @@
 
 set -eux
 
-apt-get update
-apt-get install -y build-essential
+sudo apt-get update
+sudo apt-get install -y build-essential
 curl -q -L https://github.com/edenhill/librdkafka/archive/v0.11.5.tar.gz | tar xzf -
 pushd librdkafka-0.11.5
 ./configure --prefix=/usr
 make -j
-make install
+sudo make install
 popd
 mkdir -p /root/bin
 curl https://raw.githubusercontent.com/golang/dep/master/install.sh | sh
