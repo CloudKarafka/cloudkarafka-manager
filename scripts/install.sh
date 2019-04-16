8c12f3161659b6f04fc183b98e8385ecee5ccd48
Magnus Landerblom <magnus@landerblom.se>
2019-04-16 21:52:03 +0200

    Build with a newer openssl

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,7 +3,7 @@
 set -eux
 
 sudo apt-get update
-sudo apt-get install -y build-essential openjdk-8-jre-headless
+sudo apt-get install -y build-essential openjdk-8-jre-headless openssl
 curl -q -L https://github.com/edenhill/librdkafka/archive/v0.11.5.tar.gz | tar xzf -
 pushd librdkafka-0.11.5
 ./configure --prefix=/usr
