901072143e389bee7423c237f097e22bcf43fcf6
Magnus Hörberg <magnus@84codes.com>
2020-01-07 11:26:25 +0100

    update package script

--- scripts/package.sh
+++ scripts/package.sh
@@ -12,15 +12,9 @@ md5name () {
 TEMP_DIR=target
 mkdir -p "$TEMP_DIR"
 cp -r static "$TEMP_DIR/static"
-cp -r templates "$TEMP_DIR/templates"
-rm "$TEMP_DIR/templates/templates.go"
 
 while IFS= read -r -d '' pathname; do
     test -f "$pathname" || continue
     hashed=$(md5name "$pathname")
     cp "$pathname" "${pathname%/*}/$hashed"
-    find "$TEMP_DIR"/templates/{layouts,pages} -type f -name '*.html' | xargs \
-      sed -i -e "s/${pathname##*/}/$hashed/g"
 done < <(find "$TEMP_DIR/static" -regextype sed -regex '.*\.\(css\|js\)$' -type f -print0)
-
-
