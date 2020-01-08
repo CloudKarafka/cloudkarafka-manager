0b4f6246a36a66dba86be9582ca13a06b90ac46e
Magnus Hörberg <magnus@84codes.com>
2020-01-08 11:22:37 +0100

    minor refactor

--- store/metrics.go
+++ store/metrics.go
@@ -1,145 +1,152 @@
 package store
 
 import (
-	"bytes"
-	"strings"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+
+	"github.com/cloudkarafka/cloudkarafka-manager/config"
+	"github.com/cloudkarafka/cloudkarafka-manager/log"
+	"github.com/patrickmn/go-cache"
 )
-
-type JMXBean struct {
-	Service string
-	Params  map[string]string
-}
-
-func BeanFromString(str string) JMXBean {
-	parts := strings.Split(str, ":")
-	service := parts[0]
-	params := parts[1]
-	bean := JMXBean{
-		Service: service,
-		Params:  make(map[string]string),
-	}
-	for _, p := range strings.Split(params, ",") {
-		pp := strings.Split(p, "=")
-		bean.Params[pp[0]] = pp[1]
-	}
-	return bean
-}
-
-func (b JMXBean) String() string {
-	var buffer bytes.Buffer
-	buffer.WriteString(b.Service + ":")
-	for k, v := range b.Params {
-		buffer.WriteString(k + "=" + v + ",")
-	}
-	s := buffer.String()
-	return s[:len(s)-1]
-}
 
 var (
-	BeanBrokerBytesInPerSec = JMXBean{"kafka.server", map[string]string{
-		"type": "BrokerTopicMetrics",
-		"name": "BytesInPerSec"}}
-	BeanBrokerBytesOutPerSec = JMXBean{"kafka.server", map[string]string{
-		"type": "BrokerTopicMetrics",
-		"name": "BytesOutPerSec"}}
-	BeanBrokerMessagesInPerSec = JMXBean{"kafka.server", map[string]string{
-		"type": "BrokerTopicMetrics",
-		"name": "MessagesInPerSec"}}
-	BeanBrokerBytesRejectedPerSec = JMXBean{"kafka.server", map[string]string{
-		"type": "BrokerTopicMetrics",
-		"name": "BytesRejectedPerSec"}}
-
-	BeanBrokerPartitionCount = JMXBean{"kafka.server", map[string]string{
-		"type": "ReplicaManager",
-		"name": "PartitionCount"}}
-	BeanBrokerLeaderCount = JMXBean{"kafka.server", map[string]string{
-		"type": "ReplicaManager",
-		"name": "LeaderCount"}}
-
-	BeanBrokerUnderReplicatedPartitions = JMXBean{"kafka.server", map[string]string{
-		"type": "ReplicaManager",
-		"name": "UnderReplicatedPartitions"}}
-	BeanBrokerOfflinePartitionsCount = JMXBean{"kafka.controller", map[string]string{
-		"type": "ReplicaManager",
-		"name": "OfflinePartitionsCount",
-	}}
-
-	BeanBrokerIsrShrinks = JMXBean{"kafka.server", map[string]string{
-		"type": "ReplicaManager",
-		"name": "IsrShrinksPerSec",
-	}}
-	BeanBrokerIsrExpands = JMXBean{"kafka.server", map[string]string{
-		"type": "ReplicaManager",
-		"name": "IsrExpandsPerSec",
-	}}
-	BeanBrokerConnections = JMXBean{"kafka.server", map[string]string{
-		"type":             "socket-server-metrics",
-		"listener":         "*",
-		"networkProcessor": "*"}}
-	// res, err := QueryBroker(brokerId, bean, "connection-count", "listener")
-
-	BeanAllTopicsBytesInPerSec = JMXBean{"kafka.server", map[string]string{
-		"type":  "BrokerTopicMetrics",
-		"name":  "BytesInPerSec",
-		"topic": "*"}}
-	BeanAllTopicsBytesOutPerSec = JMXBean{"kafka.server", map[string]string{
-		"type":  "BrokerTopicMetrics",
-		"name":  "BytesOutPerSec",
-		"topic": "*"}}
-
-	BeanTopicBytesInPerSec = func(topic string) JMXBean {
-		return JMXBean{"kafka.server", map[string]string{
-			"type":  "BrokerTopicMetrics",
-			"name":  "BytesInPerSec",
-			"topic": topic}}
-	}
-	BeanTopicBytesOutPerSec = func(topic string) JMXBean {
-		return JMXBean{"kafka.server", map[string]string{
-			"type":  "BrokerTopicMetrics",
-			"name":  "BytesOutPerSec",
-			"topic": topic}}
-	}
-
-	BeanAllTopicsLogStart = JMXBean{"kafka.log", map[string]string{
-		"type":      "Log",
-		"name":      "LogStartOffset",
-		"topic":     "*",
-		"partition": "*"}}
-
-	BeanAllTopicsLogEnd = JMXBean{"kafka.log", map[string]string{
-		"type":      "Log",
-		"name":      "LogEndOffset",
-		"topic":     "*",
-		"partition": "*"}}
-
-	BeanAllTopicsLogSize = JMXBean{"kafka.log", map[string]string{
-		"type":      "Log",
-		"name":      "Size",
-		"topic":     "*",
-		"partition": "*"}}
-
-	BeanTopicLogStart = func(topic string) JMXBean {
-		return JMXBean{"kafka.log", map[string]string{
-			"type":      "Log",
-			"name":      "LogStartOffset",
-			"topic":     topic,
-			"partition": "*"}}
-
-	}
-	BeanTopicLogEnd = func(topic string) JMXBean {
-		return JMXBean{"kafka.log", map[string]string{
-			"type":      "Log",
-			"name":      "LogEndOffset",
-			"topic":     topic,
-			"partition": "*"}}
-
-	}
-	BeanTopicLogSize = func(topic string) JMXBean {
-		return JMXBean{"kafka.log", map[string]string{
-			"type":      "Log",
-			"name":      "Size",
-			"topic":     topic,
-			"partition": "*"}}
-
-	}
+	jmxCache1Min = cache.New(1*time.Minute, 1*time.Minute)
+	plainCache   = cache.New(1*time.Hour, 1*time.Hour)
 )
+
+type MetricRequest struct {
+	BrokerId int
+	Bean     JMXBean
+	Attr     string
+}
+
+func (mr MetricRequest) String() string {
+	return fmt.Sprintf("%d:%s/%s", mr.BrokerId, mr.Bean, mr.Attr)
+}
+
+type MetricResponse struct {
+	Metrics []Metric
+	Error   error
+}
+
+type Metric struct {
+	Broker           int     `json:"broker"`
+	Topic            string  `json:"topic"`
+	Name             string  `json:"name"`
+	Partition        string  `json:"partition"`
+	Type             string  `json:"type"`
+	Value            float64 `json:"value"`
+	Listener         string  `json:"listener"`
+	NetworkProcessor string  `json:"networkProcessor"`
+	Attribute        string  `json:"attribute"`
+	Request          string  `json:"request"`
+	Key              string  `json:"key"`
+}
+
+func doRequest(url string) ([]Metric, error) {
+	var v []Metric
+	r, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer r.Body.Close()
+	//log.Debug("bean_request", log.MapEntry{"url": url, "status": r.StatusCode})
+	if r.StatusCode != 200 {
+		log.Warn("bean_request", log.MapEntry{"url": url, "status": r.StatusCode})
+		return nil, fmt.Errorf("URL %s returned %d", url, r.StatusCode)
+	}
+	if err = json.NewDecoder(r.Body).Decode(&v); err != nil {
+		return nil, err
+	}
+	//if len(v) == 0 {
+	//	log.Debug("bean_request", log.MapEntry{"body": "[]", "url": url})
+	//}
+	return v, nil
+}
+
+func GetMetrics(query MetricRequest) ([]Metric, error) {
+	switch query.Attr {
+	case "OneMinuteRate":
+		if r, found := jmxCache1Min.Get(query.String()); found {
+			log.Info("GetMetrics cached", log.MapEntry{"Bean": query.Bean.String(), "Attr": query.Attr})
+			return r.([]Metric), nil
+		}
+	case "TimeSerie":
+		// Never cache
+	case "Count":
+		// Never cache
+	case "Value":
+		// Never cache
+	default:
+		log.Info("GetMetrics nocache", log.MapEntry{"Attr": query.Attr})
+	}
+	host := config.BrokerUrls.HttpUrl(query.BrokerId)
+	if host == "" {
+		return nil, fmt.Errorf("Broker %d not available", query.BrokerId)
+	}
+	url := fmt.Sprintf("%s/jmx?bean=%s&attrs=%s", host, query.Bean, query.Attr)
+	v, err := doRequest(url)
+	if err == nil {
+		for i, _ := range v {
+			v[i].Broker = query.BrokerId
+		}
+	}
+	switch query.Attr {
+	case "OneMinuteRate":
+		jmxCache1Min.Set(query.String(), v, cache.DefaultExpiration)
+	}
+	return v, err
+}
+
+func GetMetricsAsync(queries []MetricRequest) <-chan MetricResponse {
+	ch := make(chan MetricResponse)
+	for _, query := range queries {
+		go func(query MetricRequest) {
+			data, err := GetMetrics(query)
+			ch <- MetricResponse{data, err}
+		}(query)
+	}
+	return ch
+}
+
+func getSimpleValue(url string) (string, error) {
+	r, err := http.Get(url)
+	if err != nil {
+		return "", err
+	}
+	if r.StatusCode != 200 {
+		return "", nil
+	}
+	body, err := ioutil.ReadAll(r.Body)
+	return string(body), err
+}
+
+func KafkaVersion(brokerId int) (string, error) {
+	key := fmt.Sprintf("kafka_version_%d", brokerId)
+	if r, found := plainCache.Get(key); found {
+		return r.(string), nil
+	}
+	res, err := getSimpleValue(fmt.Sprintf("%s/kafka-version", config.BrokerUrls.HttpUrl(brokerId)))
+	if err != nil {
+		return "", nil
+	}
+	plainCache.Set(key, res, cache.DefaultExpiration)
+	return res, nil
+}
+
+func PluginVersion(brokerId int) (string, error) {
+	key := fmt.Sprintf("plugin_version_%d", brokerId)
+	if r, found := plainCache.Get(key); found {
+		return r.(string), nil
+	}
+	res, err := getSimpleValue(fmt.Sprintf("%s/plugin-version", config.BrokerUrls.HttpUrl(brokerId)))
+	if err != nil {
+		return "", nil
+	}
+	plainCache.Set(key, res, cache.DefaultExpiration)
+	return res, nil
+
+}
