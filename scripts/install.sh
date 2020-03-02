f50208718edf07af7df7ab3117121c0081564ed7
Magnus Hörberg <magnus@84codes.com>
2020-03-02 08:57:05 +0100

    fix librdkafka dep

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,7 +4,7 @@ set -eux
 sudo apt-get update
 sudo apt-get install -y build-essential openssl
 curl -q -L https://github.com/edenhill/librdkafka/archive/v1.2.2.tar.gz | tar xzf -
-pushd librdkafka-1.2.2
+pushd librdkafka-1.3.0
 ./configure --prefix=/usr
 make -j
 sudo make install
