69d1b4ed2538083d6beaf8d2d7dab01daa52a38f
Magnus Hörberg <magnus@84codes.com>
2020-04-08 09:38:26 +0200

    set Version tag?

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,8 +3,7 @@
 set -eux
 
 export GOOS=linux
-export VERSION=0.3.1
 
-go build -ldflags "-X github.com/cloudkarafka/cloudkarafka-manager/config.GitCommit=$COMMIT -X github.com/cloudkarafka/cloudkarafka-manager/config.BuildDate=$(date)" -tags static -a -installsuffix cgo -o cloudkarafka-mgmt.linux
+go build -ldflags "-X github.com/cloudkarafka/cloudkarafka-manager/config.GitCommit=$COMMIT -X github.com/cloudkarafka/cloudkarafka-manager/config.Version=$VERSION" -tags static -a -installsuffix cgo -o cloudkarafka-mgmt.linux
 go test -v ./...
 
